cf871c66f492e42a60d0540edb884518cfb2a64b
Guilherme Luiz <margilepow1227@gmail.com>
2025-04-25 16:53:35 -0300

    estruturas

---
+++ 2_ESTRUTURAS/js/script.js
@@ -0,0 +1,28 @@
+// PONTO E VÍRGULA É OPCIONAL(poucas vezes é obrigatório)
+
+// 1 - variáveis 
+let nome = "Guilherme"
+console.log(nome)
+nome = "Guilherme Luiz"
+console.log(nome)
+
+const idade = 18
+console.log("Olá, eu me chamo " + nome + " e tenho " + idade + "anos")
+
+// 2 - mais sobre variáveis 
+
+// let 2teste = "inválido"
+// let @nome = "Guilherme"
+let a = 10, b = 20, c = 30
+console.log(a, b, c)
+
+const nomecompleto = "Guilherme Luiz"
+const nomeCompleto = "Leandro Luiz"
+
+console.log(nomecompleto)
+console.log(nomeCompleto)
+
+let _teste = "teste ok!"
+let $teste = "teste ok!"
+
+console.log(_teste, $teste)
