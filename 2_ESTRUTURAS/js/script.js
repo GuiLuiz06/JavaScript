8f5ce607d72f6937e44903dbb87674cdd5c98c46
Guilherme Luiz <margilepow1227@gmail.com>
2025-04-30 16:46:04 -0300

    novos

--- 2_ESTRUTURAS/js/script.js
+++ 2_ESTRUTURAS/js/script.js
@@ -26,3 +26,81 @@ let _teste = "teste ok!"
 let $teste = "teste ok!"
 
 console.log(_teste, $teste)
+
+// 3 - prompt
+// const age = prompt("digite a sua idade: ")
+
+// console.log(`Você tem ${age} anos`)
+
+// 4 - alert 
+// alert("Testando")
+
+// const z = 10;
+
+// alert(`o número é ${z}`)
+
+// 5 - math 
+console.log(Math.min(5, 2, 1, 10))
+console.log(Math.max(5, 2, 1, 10))
+console.log(Math.floor(5.14))
+console.log(Math.ceil(5.14))
+
+// 6 - console
+console.log("Teste!")
+console.error("Erro!")
+console.warn("aviso!")
+
+// ESTRUTURAS DE CONTROLE
+
+// if 
+
+const m = 10;
+
+if(m > 5) {
+    console.log("Seu número é maior que 5!")
+}
+
+const user = "joão"
+
+if(user === "joão") {
+    console.log(`Olá, ${user}, é um prazer lhe conhecer.`)
+}
+
+if(user === "Maria") {
+    console.log("Olá Maria!")
+}
+
+console.log(user === "joão", user === "Maria")
+
+// if else
+
+const usuario = "caio"
+if (usuario === "Guilherme") {
+    console.log(`Olá, ${usuario}, é um prazer ter a sua presença conosco.`)
+}
+else{
+    console.log(`Olá, ${usuario}, por favor se retire, você não é o Guilherme.`)
+}
+
+// else if
+if(10 > 11) {
+    console.log("Teste 1")
+} 
+else if (15 > 16) {
+    console.log("Teste 2") 
+}
+else if(20 > 10) {
+    console.log("Agora vai!!")
+}
+
+const username = "caio"
+const idadeG = 18
+
+if(username === "Guilherme") {
+    console.log("Bem vindo Caio!")
+} else if(username === "Guilherme" && idadeG === 18) {
+     console.log("Olá Guilherme, você tem 18 anos!")
+}
+else {
+    console.log("Nenhuma condição aceita!")
+}
