638b28c4ac65aca501f757c6669c52c2d9826056
Guilherme Luiz <margilepow1227@gmail.com>
2025-05-09 16:42:18 -0300

    obj e arrays

--- JavaScript-main/4_ARRAYS_&_OBJ/js/script.js
+++ JavaScript-main/4_ARRAYS_&_OBJ/js/script.js
@@ -33,3 +33,78 @@ const meunome = "Guilherme"
 
 console.log(meunome.toUpperCase())
 console.log(meunome.indexOf("m"))
+
+// 5 - objetos
+const person = {
+    name: "Guilherme", 
+    age: 18,
+    job: "Desenvolvedor Web"
+}
+console.log(person)
+console.log(person.job.length)
+
+// 6 - criando e deletando propriedades  
+const pc = {
+    process: 4.5,
+    memoria: "16gb",
+    font: "650W"
+}
+console.log(pc)
+
+pc.fans = 4
+
+console.log(pc)
+
+// delete pc.font
+// console.log(pc) 
+
+// arrays - listas de itens 
+// Objetos - são utilizados para descrever um tem
+// é possivel ter um array de objetos
+
+// 7 - mais sobre objetos
+ const obj = {
+    a: "Ola",
+    b: true,
+ }
+
+ console.log(obj instanceof Object)
+
+ const obj2 = {
+    c: ["gui"],
+ }
+
+ Object.assign(obj2, obj)
+ console.log(obj2)
+ console.log(obj)
+
+//   8 - conhecendo os objetos
+console.log(Object.keys(pc))
+console.log(Object.entries(pc))
+
+// 9 - mutação
+const a = {
+    name: "Guilherme",
+}
+const b = a
+
+console.log(a)
+console.log(b)
+
+console.log(a === b)
+
+a.age = 18;
+
+console.log(b)
+
+delete b.age
+
+console.log(a)
+console.log(b)
+
+// 10 -loop em array
+const nomes = ["pedro", "lucas", "mateus", "guilherme"]
+
+for (let i = 0; i < nomes.length; i++) {
+    console.log(`Listando os usuários: ${nomes[i]}`)
+}
