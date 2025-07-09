e70c34b40c54fce146c3b1c7947f71d96cad2195
Guilherme Luiz <margilepow1227@gmail.com>
2025-07-09 15:58:37 -0300

    new events

--- JavaScript-main/9_EVENTOS/js/script.js
+++ JavaScript-main/9_EVENTOS/js/script.js
@@ -1,7 +1,68 @@
 // 1 - adicionando eventos
-const btn = document.querySelector("#my-buttom")
+const btnSecond = document.querySelector("#my-buttom")
 
-btn.addEventListener("click", function() {
+btnSecond.addEventListener("click", function() {
 
     console.log("clicastes aqui")
+})
+
+// 2 - removendo eventos
+const btn = document.querySelector("#btn")
+
+function imprimindoMsg() {
+    console.log("clicou")
+}
+btn.addEventListener("click", imprimindoMsg)
+
+const threeBtn = document.querySelector("#btn-other")
+
+threeBtn.addEventListener("click", () => {
+    console.log("O evento foi removido com sucesso!")
+    btn.removeEventListener("click", imprimindoMsg)
+})
+
+// 3 - argumento do evento
+const myTitle = document.querySelector("#my-title")
+
+myTitle.addEventListener("click", (event) => {
+    console.log(event)
+    console.log(event.offsetX)
+    console.log(event.pointerType)
+    console.log(event.target)
+})
+
+// 4 - propagação
+const containerBtn = document.querySelector("#container-btn")
+const bntInsertdiv = document.querySelector("#btn-div")
+
+containerBtn.addEventListener("click", () => {
+    console.log("evento 1")
+})
+bntInsertdiv.addEventListener("click", (e) => {
+    e.stopPropagation()
+    console.log("evento 2")
+})
+
+// 5 - removendo evento padrão 
+const a = document.querySelector("a")
+
+a.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    console.log("removi o evento padrão de ir para o google")
+})
+
+// 6 - evento de tecla
+document.addEventListener("keyup", (e) => {
+    console.log(`você apertou o ${e.key}`)
+})
+document.addEventListener("keyup", (e) => {
+    console.log(`você soltou o ${e.key}`)
+})
+
+// 7 - eventos de mouse
+const eventMouse = document.addEventListener("#mouse")
+
+eventMouse.addEventListener("mousedown", () => {
+    console.log(`segurou o botão do mouse`)
 })
