13f5727d46dc33939e6e2a6c3780aa6ecd2352e7
Guilherme Luiz <margilepow1227@gmail.com>
2025-12-03 16:26:33 -0300

    new

--- study-JavaScript/5_OOP/js/script.js
+++ study-JavaScript/5_OOP/js/script.js
@@ -105,4 +105,85 @@ const meuSegundoObjeto = Object.create(meuObjeto)
 
 console.log(meuSegundoObjeto)
 
-// 4 - classes
+// 4 - classes básicas 
+const cachorro = {
+    raca: null,
+    patas: 4
+}
+
+const picher = Object.create(cachorro)
+
+picher.raca = "pincher"
+
+console.log(picher)
+
+console.log(picher.patas)
+
+const pitBull = Object.create(cachorro)
+
+pitBull.raca = "PitBull"
+
+console.log(pitBull)
+
+// 4 - função como classe - função construtora
+function criarCachorro(nome, raca) {
+
+    const cachorro = Object.create({})
+    cachorro.nome = nome
+    cachorro.raca = raca
+
+    return cachorro
+}
+
+const greg = criarCachorro("Greg", "bulldog")
+const lick = criarCachorro("Lick", "PitBull")
+console.log(greg)
+
+console.log(Object.getPrototypeOf(lick))
+
+// exercicios
+function criarHumano(nome, idade) {
+    const humano = Object.create({})
+    humano.nome = nome
+    humano.idade = idade
+
+    return humano
+}
+
+const luiz = criarHumano("Luiz", 19)
+
+console.log(luiz)
+
+// 5 - funcoes como classe 
+
+function Criarhumano(nome, idade) {
+    this.nome = nome
+    this.idade = idade
+}
+
+const gui = new Criarhumano("Guilherme", 19)
+
+console.log(gui)
+
+
+// 6 - metodos na função construtora
+Criarhumano.prototype.uivar = function () {
+    console.log("AUUUU!")
+}
+
+console.log(Criarhumano.prototype)
+
+gui.uivar()
+
+// 7 - classes es6
+class Humanocriar {
+    constructor(nome, idade) {
+        this.nome = nome
+        this.idade = idade
+    }
+}
+
+const jorge = new Humanocriar("Jorge", 44)
+
+console.log(jorge)
+console.log(Object.getPrototypeOf(jorge))
